c6c209a546b9b60747320b97cae3dae96e84f690
jack javi <jackmtembete@gmail.com>
2024-01-11 02:10:50 +0000

    Student class.

--- 0x04-Typescript/task_1/js/main.ts
+++ 0x04-Typescript/task_1/js/main.ts
@@ -27,3 +27,27 @@ export interface IStudentClass {
   workOnHomework(): string;
   displayName(): string;
 }
+
+export class StudentClass implements IStudentClass {
+  private _firstName!: string;
+  private _lastName!: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this._firstName;
+  }
+}
+
+export function createStudent(ctor: IStudentClassConstructor, firstName: string, lastName: string): IStudentClass {
+  return new ctor(firstName, lastName);
+}
+
+
